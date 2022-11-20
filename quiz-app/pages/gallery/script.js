86f942dbb0137139d4a0aaf405d9798db0c92976
Maria Gornashevich <lightbrown9874@gmail.com>
2022-11-21 01:23:29 +0300

    feat: add new page

--- quiz-app/pages/gallery/script.js
+++ quiz-app/pages/gallery/script.js
@@ -13,11 +13,34 @@ cosmosDataArr.forEach((el) => {
 console.log(galleryArr);
 
 gallery.innerHTML = `
-    <div class="parent">
-        <div class="child">a</div>
-        <div class="child">+</div>
-        <div class="child">b</div>
-        <div class="child">=</div>
-        <div class="child">a+b</div>
+<div class="gallery__item">
+<div class="item__block">
+  <div class="item__img"></div>
+  <div class="item__info">
+    <p class="item__name"></p>
+    <p class="item__astronomical-body">klknl</p>
+    <div class="item__audio-play">
+      <div class="item__secret-play">
+        <div class="item__play"></div>
+
+        <input
+          type="range"
+          class="item__progress-bar"
+          min="0"
+          max="86"
+          value="0"
+        />
+        <div class="item__currentTime"></div>
+        <div class="item__durationTime"></div>
+      </div>
     </div>
+  </div>
+</div>
+<p class="item__description"></p>
+</div>
+<div class="gallery__item"></div>
+<div class="gallery__item"></div>
+<div class="gallery__item"></div>
+<div class="gallery__item"></div>
+<div class="gallery__item"></div>
 `;
