2e127a2c132c652ca3a178d01dfa37ba406206d3
Maria Gornashevich <lightbrown9874@gmail.com>
2022-11-21 23:53:19 +0300

    feat: add result page logic

--- quiz-app/pages/results/script.js
+++ quiz-app/pages/results/script.js
@@ -1,9 +1,20 @@
 const resultsBtn = document.querySelector(".results__btn");
 const resultsScore = document.querySelector(".results__score");
-resultsBtn.addEventListener(
-  "click",
-  () => (window.location.href = "../game/index.html")
-);
+
+resultsBtn.disable = false;
 
 const score = localStorage.getItem("score");
+
 resultsScore.innerHTML = score;
+
+if (resultsScore.innerHTML === '30') {
+  resultsBtn.innerHTML = "GAME OVER";
+  resultsBtn.disable = true;
+} else {
+  resultsBtn.innerHTML = "Play again";
+  resultsBtn.addEventListener("click", () => {
+    window.location.href = "../game/index.html";
+  });
+}
+
+
