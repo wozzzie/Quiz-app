556aed8952f9209b1eb01e998e949c4b8b7fea8e
Maria Gornashevich <lightbrown9874@gmail.com>
2022-11-21 17:35:38 +0300

    feat: add styles for gallery

--- quiz-app/pages/gallery/script.js
+++ quiz-app/pages/gallery/script.js
@@ -1,9 +1,8 @@
-import cosmosData from "../data.js";
+import galleryData from "../gallery.js";
 
 const galleryItem = document.querySelectorAll(".gallery__item");
 const gallery = document.querySelector(".gallery");
 
-const cosmosDataArr = cosmosData.flat();
 const galleryArr = [];
 let galleryAudio = new Audio();
 let isGalleryPlay = false;
@@ -38,48 +37,29 @@ function getTimeCodeFromNum(num) {
 //   audio.currentTime = progressBar.value;
 // });
 
-cosmosDataArr.forEach((el) => {
-  if (el.id === 1) {
-    galleryArr.push(el);
-  }
+galleryData.forEach((el) => {
+  galleryArr.push(el);
 });
 console.log(galleryArr);
 
-gallery.innerHTML = galleryArr.map((el) => {
-  return `
-  <div class="gallery__item">
-  <div class="item__block">
-    <img class="item__img" src="${el.image}"></img>
-    <div class="item__info">
-      <p class="item__name">${el.name}</p>
-      <p class="item__astronomical-body">${el.astronomicalBodies}</p>
-    
-    </div>
-    
-  </div>
-  <audio
-  class="item__audio"
-  controls
-  src="${el.audio}">
-</audio>
-  <p class="item__description">${el.description}</p>
-  </div>
-  `;
-});
+// gallery.innerHTML = galleryArr.map((el) => {
+//   return `
+//   <div class="gallery__item">
+//   <div class="item__block">
+//     <img class="item__img" src="${el.image}"></img>
+//     <div class="item__info">
+//       <p class="item__name">${el.name}</p>
+//       <p class="item__astronomical-body">${el.astronomicalBodies}</p>
 
-{
-  /* <div class="item__audio-play">
-<div class="item__secret-play">
-  <div class="item__play"></div>
-  <input
-    type="range"
-    class="item__progress-bar"
-    min="0"
-    max="86"
-    value="0"
-  />
-  <div class="item__currentTime"></div>
-  <div class="item__durationTime"></div>
-</div>
-</div> */
-}
+//     </div>
+
+//   </div>
+//   <audio
+//   class="item__audio"
+//   controls
+//   src="${el.audio}">
+// </audio>
+//   <p class="item__description">${el.description}</p>
+//   </div>
+//   `;
+// });
